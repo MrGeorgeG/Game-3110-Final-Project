2867f86ee38d252a5644a6fa3fd15f430109a71d
MrGeorgeG <georgegan94@gmail.com>
2020-10-18 13:55:58 -0400

    TankExplesion
    Fix

--- Assets/Scripts/DestroyForTime.cs
+++ Assets/Scripts/DestroyForTime.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyForTime : MonoBehaviour
 {
-    public float time = 1.5f;
+    public float time = 1.0f;
 
     // Start is called before the first frame update
     void Start()
